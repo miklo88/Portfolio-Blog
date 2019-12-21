16052b28d6adfdd50a13dfb1e0ebd80ed6551f4e
Carlos <miklo.concepcion@gmail.com>
2019-12-21 16:19:56 -0500

    finished building in app.js file. building components for routes to go to

--- React-Router/routing-paths/src/App.js
+++ React-Router/routing-paths/src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { render } from "react-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hola Bienvendidios!! Me llamo Carlos Adrian!</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <p>Hola Bienvendidios!! Welcome to my React-Router!</p>
+      </div>
+      <ul>
+        <li>
+          <Link to='/one'>One</Link>
+        </li>
+        <li>
+          <Link to='/two'>Two</Link>
+        </li>
+        <li>
+          <Link to='/three'>Three</Link>
+        </li>
+        <li>
+          <Link to='/four'>Four</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <RouteWrapper path='/one' component={One} />
+        <RouteWrapper path='/two' component={Two} />
+        <RouteWrapper path='/three' component={Three} />
+        <RouteWrapper path='/four' component={Four} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
