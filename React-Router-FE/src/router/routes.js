d27811e72c660c5eca8ba1b65a5a1e65acb6b3fc
Carlos <miklo.concepcion@gmail.com>
2020-02-15 14:34:15 -0600

    adding basic scss styling to the router and routes

--- React-Router-FE/src/router/routes.js
+++ React-Router-FE/src/router/routes.js
@@ -6,13 +6,16 @@ import { One } from "../components/one";
 import { Two } from "../components/two";
 import { Three } from "../components/three";
 import { Four } from "../components/four";
+// stylez jeje
+import "./Routes.scss";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <div className='App'>
-        <p>Hola Bienvendidios!! Welcome to my React-Router!</p>
+      <div className='Routes-Container'>
+        <h1>Hola Bienvendidios!! Welcome to my React-Router!</h1>
       </div>
+
       <ul>
         <li>
           <Link to='/one'>Page One</Link>
