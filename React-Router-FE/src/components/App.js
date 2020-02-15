83d5e1b4fd2557ba8b7388eea7478e00b809a577
Carlos <miklo.concepcion@gmail.com>
2020-02-15 13:54:08 -0600

    back in the routing of this front end. cleaning everything up and re-routing this front end. goal is to add a login-signup component.

---
+++ React-Router-FE/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// imported local files
+import Routes from "../router/routes";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+export default App;
---
+++ React-Router-FE/src/components/three.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Three = () => {
+  return (
+    <div>
+      <h1>Content of page three</h1>
+    </div>
+  );
+};
+
+export { Three };
