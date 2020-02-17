250375528229a0d527bc340c182b841e74aa825e
Carlos <miklo.concepcion@gmail.com>
2020-02-17 08:59:43 -0600

    still implementing redux. establishing store and the reducers and actions. lots of redux work to do.

--- React-Router-FE/src/login/login.js
+++ React-Router-FE/src/login/login.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 // STYLES
 import "../styles/Login.scss";
 
 const Login = () => {
+  const [email, setEmail] = useState("email@email.com");
+  const [password, setPassword] = useState("test123");
+
   return (
     <div className='Login-Component'>
       <h1 className='Page-Title'>Login</h1>
-      <form className='Login-Form'>
+      <form className='Login-Form' onSubmit={useState.value}>
         <label className='Input-Label'>
           email:
-          <input className='Input-Field' type='text' text='email' />
+          <input
+            className='Input-Field'
+            name='email'
+            value=''
+            onChange={email}
+          />
         </label>
         <label className='Input-Label'>
           password:
-          <input className='Input-Field' type='text' text='password' />
+          <input
+            className='Input-Field'
+            name='password'
+            value=''
+            onChange={password}
+          />
         </label>
-        <label className='Input-Label'>
-          <input className='Input-Field' type='submit' value='Submit' />
-        </label>
+        <button
+          className='Button-Label'
+          onClick={(() => setEmail, setPassword)}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
