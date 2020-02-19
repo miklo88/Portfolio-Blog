7ef8e43e489eb926ee0c01331d8cca68f0dd3160
Carlos <miklo.concepcion@gmail.com>
2020-02-19 16:19:39 -0600

    styling set. thinkbaout changes. going back to login logic.

--- React-Router-FE/src/components/photos.js
+++ React-Router-FE/src/components/photos.js
@@ -5,8 +5,8 @@ import "../styles/Photos.scss";
 const Photos = () => {
   return (
     <div className='Photos-Component'>
-      <div className='Photo-Container'>
-        <h1 className='Page-Title'>PHOTOGRAPHY</h1>
+      <div className='Photo-Image'>
+        <h1 className='Page-Title'>PHOTOS</h1>
       </div>
     </div>
   );
