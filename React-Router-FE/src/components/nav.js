19a51b5fa1fea5bf97bd983720a46f176a9ca18f
Carlos <miklo.concepcion@gmail.com>
2020-02-21 19:43:06 -0600

    styles cleaned up in design.scss and photos.scss. going to login.scss and cleaning up that folder as well. then along with login i'm implementing auth0 to this application

--- React-Router-FE/src/components/nav.js
+++ React-Router-FE/src/components/nav.js
@@ -6,25 +6,22 @@ export default function Nav() {
   return (
     <div className='Nav'>
       <h1 className='Main-Title'>MIKLO</h1>
-      <ul className='Nav-Container'>
+      <ul className='Nav-Menu'>
         <li className='Nav-Link'>
           <Link className='Link' to='/login'>
             Login
           </Link>
         </li>
-
         <li className='Nav-Link'>
           <Link className='Link' to='/about'>
             About
           </Link>
         </li>
-
         <li className='Nav-Link'>
           <Link className='Link' to='/photos'>
             Photography
           </Link>
         </li>
-
         <li className='Nav-Link'>
           <Link className='Link' to='/design'>
             Design
