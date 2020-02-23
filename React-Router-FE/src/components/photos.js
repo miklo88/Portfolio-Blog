ab4066ee67d5653591a31af17470c224a6ded6e3
Carlos <miklo.concepcion@gmail.com>
2020-02-22 20:54:18 -0600

    added more photos. also titles for images. making this look a lot better meow

--- React-Router-FE/src/components/photos.js
+++ React-Router-FE/src/components/photos.js
@@ -1,14 +1,24 @@
 import React from "react";
 // STYLES
 import "../styles/Photos.scss";
-import photo from "../stockPhotos/art14blk_white15.JPG";
+import photo1 from "../stockPhotos/blk_whtie1blk_white15_1.JPG";
+import photo2 from "../stockPhotos/portBW1.JPG";
 // import photo from "../stockPhotos/v1.46.JPG";
 const Photos = () => {
   return (
     <div className='P-Component'>
       <div className='P-Page'>
         <h1 className='Page-Title'>Photography</h1>
-        <img className='P-Image' src={photo} alt='' />
+        <img className='P-Image' src={photo1} alt='' />
+        <p className='P-Title'>"Private Dining Room" </p>
+        <p className='P-Title'>mk the restaurant</p>
+        <p className='P-Location'>Chicago, IL </p>
+      </div>
+      <div className='P-Page'>
+        <img className='P-Image' src={photo2} alt='' />
+        <p className='P-Title'>"El Tiede" </p>
+        <p className='P-Title'>Tiede Volcano</p>
+        <p className='P-Location'>Tenerife, Islas de Las Canarias Espana </p>
       </div>
     </div>
   );
