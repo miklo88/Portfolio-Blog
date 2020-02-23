0ae0136e4a07118c731a089a939fc6064198a081
Carlos <miklo.concepcion@gmail.com>
2020-02-23 00:42:18 -0600

    just added a whole bunch of photos

--- src/components/design.js
+++ src/components/design.js
@@ -2,7 +2,7 @@ import React from "react";
 // STYLES
 import "../styles/Design.scss";
 import photo1 from "../stockPhotos/PIv2.0 copy.jpg";
-import photo2 from "../stockPhotos/sangriav1.8.jpg";
+import photo2 from "../stockPhotos/coffee L logo v1.1.jpg";
 import photo3 from "../stockPhotos/sangriav1.8.jpg";
 
 const Design = () => {
