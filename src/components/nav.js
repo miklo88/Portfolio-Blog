f378831f39544e5b5f2651cee390a9cb9b9f13bc
Carlos <miklo.concepcion@gmail.com>
2020-03-02 09:23:10 -0600

    tweaking useState and guest signin/login

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.scss";
-
+import Clock from "../clock/clock";
 export default function Nav() {
   return (
     <div className='Nav'>
       <h1 className='Main-Title'>Miklo</h1>
       <ul className='Nav-Menu'>
         {/* NAVIGATION LINKS AND NAV-LINK CONTAINERS */}
-        <li className='Nav-Link'>
-          <Link className='Link' to='/'>
-            Blog
-          </Link>
-        </li>
+
         <li className='Nav-Link'>
           <Link className='Link' to='/about'>
             About
@@ -25,10 +21,18 @@ export default function Nav() {
         </li>
         <li className='Nav-Link'>
           <Link className='Link' to='/blog'>
-            Blog userState
+            Blog
+          </Link>
+        </li>
+        <li className='Nav-Link'>
+          <Link className='Link' to='/'>
+            Sign Up / Login
           </Link>
         </li>
       </ul>
+      <div className='Nav-timeStyle'>
+        <Clock />
+      </div>
     </div>
   );
 }
--- src/users/guestProfile.js
+++ src/users/guestProfile.js
@@ -5,6 +5,8 @@ export default class AddGuest extends Component {
     return (
       <div className='Guest-Profile'>
         <input type='text' ref='input' />
+        <input type='text' ref='input' />
+
         <button onClick={e => this.handleClick(e)}>Add</button>
         <p>Beinvenidos!</p>
       </div>
