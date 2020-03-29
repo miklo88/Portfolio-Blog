dc1b7fdce6b0e9999a127a2b400ccd14ceb4d838
Carlos <miklo.concepcion@gmail.com>
2020-03-28 19:07:15 -0500

    grid working. now time to break everything

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
-// import 'Dashboard.scss'
+import "./Dashboard.scss";
 
 function Dashboard() {
   return (
     <div className='dashboard-container'>
-      <p>A home page for now. Landing page as you suppose.</p>
+      <p className='dashboard-text'>Column 1</p>
+      <p className='dashboard-text2'>Column 2</p>
+      <p className='dashboard-text3'>Column 3</p>
     </div>
   );
 }
