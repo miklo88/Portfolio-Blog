de320bfee4f150d98afb318cfe641a9c5d4d99e0
Carlos <miklo.concepcion@gmail.com>
2020-03-30 12:38:25 -0500

    super basic 3x3 grid set up. now having fun with font styling and images.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React from "react";
-import aboutIMG from "../stockPhotos/portBW1.JPG";
+import dashIMG from "../stockPhotos/portBW1.JPG";
 import "./Dashboard.scss";
 
 function Dashboard() {
   return (
     <div className='dashboard-container'>
-      <img className='about-img' src={aboutIMG} alt='img' />
+      <img className='dashboard-img' src={dashIMG} alt='dash-img' />
+      <h1 className='dashboard-h1'>MIKLO</h1>
     </div>
   );
 }
