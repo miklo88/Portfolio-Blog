4912f3ba635b3d6f05e9269f524a436a5699f07b
Carlos <miklo.concepcion@gmail.com>
2020-03-30 17:28:20 -0500

    tweaking buttons styles a little bit.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,13 @@ function Dashboard() {
   return (
     <div className='dashboard-container'>
       <img className='dashboard-img' src={dashIMG} alt='dash-img' />
-      <h1 className='dashboard-h1'>MIKLO</h1>
+
+      <h1 className='dashboard-h1'>
+        MIKLO
+        <p className='dashboard-p'>
+          Visual Design<br></br>Photography<br></br>Chicago, IL
+        </p>
+      </h1>
     </div>
   );
 }
