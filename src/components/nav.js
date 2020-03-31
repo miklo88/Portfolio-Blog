0f6e48383592f0cba646bce4561d789dca147090
Carlos <miklo.concepcion@gmail.com>
2020-03-31 01:46:48 -0500

    cleaned up navigation. added home path to h1 in navigation. created new links to a blog component and a login/signup regristration form.

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,23 +4,25 @@ import "./Nav.scss";
 
 function Nav() {
   return (
-    <nav className='nav-container'>
-      <h1 className='nav-h1'>MIKLO</h1>
+    <div className='nav-container'>
+      {/* <h1 className='nav-h1'>MIKLO</h1> */}
+      <Link className='home-link' to='/'>
+        <h1>MIKLO</h1>
+      </Link>
       {/* links for other pages */}
-      <ul className='navigation'>
-        <li className='link-container'>
-          <Link className='links' to='/'>
-            Home
-          </Link>
-        </li>
-        <li className='link-container'>
-          <Link className='links' to='/about'>
-            About
-          </Link>
-        </li>
+      <nav className='navigation'>
+        <Link className='links' to='/about'>
+          About
+        </Link>
+        <Link className='links' to='/blog'>
+          Blog
+        </Link>
+        <Link className='links' to='/login'>
+          Login/Signup
+        </Link>
         {/* <li className='link'><Link to='/users' >Users</Link></li> */}
-      </ul>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
