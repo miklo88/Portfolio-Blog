e506176768d2569093d73baa0af3e7103eb27d15
Carl Redding <credding07@gmail.com>
2020-04-25 17:35:12 -0500

    updated index.js with importing redux and wrappin the app component in the redux provider.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducers/combine-reducers";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={createStore}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
