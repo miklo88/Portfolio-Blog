21c1f0ec103eec63d2f6f910809433a0956573fc
Carl Redding <credding07@gmail.com>
2020-04-25 20:58:44 -0500

    local state for creating a user object has been done. Now onto more in depth state management and issuing a token for the user. Then login and dynamic routing.

--- src/components/register/Signup.js
+++ src/components/register/Signup.js
@@ -1,52 +1,92 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
+import React from "react";
+// import { connect } from "react-redux";
+// import { addUser } from "../../redux/actions/User-Actions";
 import { Link } from "react-router-dom";
 import "./Signup.scss";
 
-const Signup = (props) => {
-  const [user, setUser] = useState({
-    firstName: "",
-    lastName: "",
-    userName: "",
-    email: "",
-    password: "",
-  });
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      userName: "",
+      email: "",
+      password: "",
+    };
+  }
 
-  const [submitted, setSubmitted] = useState(false);
+  registerUser() {
+    console.log("this.state", this.state);
+  }
 
-  return (
-    <>
-      <form className='signup-form'>
-        <label className='input-label'>First Name:</label>
-        <input className='input-field' type='text' placeholder='First Name' />
-        <label className='input-label'>Last Name:</label>
-        <input className='input-field' type='text' placeholder='Last Name' />
-        <label className='input-label'>Username:</label>
-        <input className='input-field' type='text' placeholder='Username' />
-        <label className='input-label'>Email:</label>
-        <input
-          className='input-field'
-          type='text'
-          placeholder='Email@gmail.com'
-        />
-        <label className='input-label'>Password:</label>
-        <input className='input-field' type='text' placeholder='Password' />
-        <button className='submit-btn' type='submit' value='submit'>
-          Submit
-        </button>
-        <div className='link-to-login'>
-          <p className='to-login'>Already a user?</p>
-          <Link className='login-link' to='/login'>
-            Login
-          </Link>
-        </div>
-      </form>
-    </>
-  );
-};
-export default Signup;
-// const mapStateToProps = state => ({});
+  render() {
+    return (
+      <div>
+        <form className='signup-form'>
+          <label className='input-label'>First Name:</label>
+          <input
+            className='input-field'
+            type='text'
+            onChange={(event) =>
+              this.setState({ firstName: event.target.value })
+            }
+            placeholder='First Name'
+          />
+          <label className='input-label'>Last Name:</label>
+          <input
+            className='input-field'
+            type='text'
+            onChange={(event) =>
+              this.setState({ lastName: event.target.value })
+            }
+            placeholder='Last Name'
+          />
+          <label className='input-label'>Username:</label>
+          <input
+            className='input-field'
+            type='text'
+            onChange={(event) =>
+              this.setState({ userName: event.target.value })
+            }
+            placeholder='Username'
+          />
+          <label className='input-label'>Email:</label>
+          <input
+            className='input-field'
+            type='text'
+            onChange={(event) => this.setState({ email: event.target.value })}
+            placeholder='Email@email.com'
+          />
+          <label className='input-label'>Password:</label>
+          <input
+            className='input-field'
+            type='password'
+            onChange={(event) =>
+              this.setState({ password: event.target.value })
+            }
+            placeholder='Password'
+          />
+          <button
+            className='submit-btn'
+            type='button'
+            onClick={() => this.registerUser()}
+          >
+            Sign Up
+          </button>
 
-// const mapDispatchToProps = {};
+          {/* to login component if user is already registered. */}
+          <div className='link-to-login'>
+            <p className='to-login'>Already a user?</p>
+            <Link className='login-link' to='/login'>
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+// export default connect()(SignUp);
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default SignUp;
