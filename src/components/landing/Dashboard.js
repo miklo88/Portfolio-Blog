0661c18ea4fb936484d5ff751bbf6f92efdd1e50
Carl Redding <credding07@gmail.com>
2020-04-29 23:00:53 -0500

    tweaking the styling per component on each category. have the landing img and text pretty much home just need to make mobile first approach and then apply that to the other categories as well.

--- src/components/landing/Dashboard.js
+++ src/components/landing/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import landing from "../../stockPhotos/discovery.jpg";
 import about from "../../stockPhotos/dame.png";
 import blog from "../../stockPhotos/dame.png";
 import drink from "../../stockPhotos/PENECILLIN.jpg";
@@ -9,23 +10,35 @@ import "./Dashboard.scss";
 const Dashboard = () => {
   return (
     <div className='dashboard-container'>
-      <p className='dash-p-landing'>
-        Web Developing<br></br>
-        Visual Design<br></br>
-        Chicago, IL
-      </p>
-      <img className='dashboard-img-about' src={about} alt='dash-img' />
-      <Link className='dash-link-about' to='/blog'>
-        About
-      </Link>
-      <img className='dashboard-img-blog' src={blog} alt='dash-img' />
-      <Link className='dash-link-blog' to='/blog'>
-        Blog
-      </Link>
-      <img className='dashboard-img-contact' src={drink} alt='dash-img' />
-      <Link className='dash-link-contact' to='/blog'>
-        Contact
-      </Link>
+      <div className='landing'>
+        <img className='dashboard-img-landing' src={landing} alt='dash-img' />
+        <p className='dash-p-landing'>
+          Web Developing<br></br>
+          Visual Design<br></br>
+          Chicago, IL
+        </p>
+      </div>
+
+      <div className='about'>
+        <img className='dashboard-img-about' src={about} alt='dash-img' />
+        <Link className='dash-link-about' to='/blog'>
+          About
+        </Link>
+      </div>
+
+      <div className='blog'>
+        <img className='dashboard-img-blog' src={blog} alt='dash-img' />
+        <Link className='dash-link-blog' to='/blog'>
+          Blog
+        </Link>
+      </div>
+
+      <div className='contact'>
+        <img className='dashboard-img-contact' src={drink} alt='dash-img' />
+        <Link className='dash-link-contact' to='/blog'>
+          Contact
+        </Link>
+      </div>
     </div>
   );
 };
