a6c6df8617105a71bcb44ce001b181fe3eab1e50
Carl Redding <credding07@gmail.com>
2020-05-03 14:59:08 -0500

    laying out the basic content of the page and what i want it to do. I should get figma open. and figure out how to establish a link on the page that someone can download.

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./About.scss";
 
-function About() {
+const About = () => {
   return (
     <div className='about-container'>
       <p className='about-text'>something about me.</p>
+      {/* what should i put on this page? */}
+      {/* a lil blurb about me, my resume and a link to download my resume. */}
+
       <Link className='home-link' to='/'>
         Home
       </Link>
     </div>
   );
-}
+};
 
 export default About;
