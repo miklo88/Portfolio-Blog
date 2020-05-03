43b9406087ec0631d6e1b6c56ffba47f824d2389
Carl Redding <credding07@gmail.com>
2020-05-03 16:15:44 -0500

    created folder in public folder with my resume in pdf and word doc forms. they both render on the page and both files work when downloading.

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./About.scss";
 
 const About = () => {
   return (
     <div className='about-container'>
+      {/* page title */}
+      <h1>Hi!, I'm Carl!</h1>
+      {/* about me blurb */}
       <p className='about-text'>something about me.</p>
+      {/* resume download link */}
+      <a href='resume/CARL-REDDING-RESUME.pdf' download='CarlsResume.pdf'>
+        PDF Resume
+      </a>
+      <a href='resume/CARL-REDDING-RESUME.docx' download='CarlsResume.docx'>
+        Word Doc Resume
+      </a>
+
       {/* what should i put on this page? */}
       {/* a lil blurb about me, my resume and a link to download my resume. */}
-
-      <Link className='home-link' to='/'>
-        Home
-      </Link>
     </div>
   );
 };
