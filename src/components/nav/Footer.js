855f3fba9bfb0af917e31895f1ca89e82cd0aeae
Carl Redding <credding07@gmail.com>
2020-05-04 12:04:45 -0500

    bringing in the icons and establishing linked functionality then styling.

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -4,6 +4,9 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className='footer-container'>
+      <i class='fab fa-linkedin-in'></i>
+      <i class='fab fa-github'></i>
+      <i class='fab fa-twitter'></i>
       <p className='footer-text'>footer component.</p>
     </div>
   );
