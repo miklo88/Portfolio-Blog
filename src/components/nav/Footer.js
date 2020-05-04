955433b6fc209ff28ee6fe91c84596e49e363b8d
Carl Redding <credding07@gmail.com>
2020-05-04 13:30:02 -0500

    icons placed in footer where i want them. adding links to my social media then styling effects.

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -4,10 +4,11 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className='footer-container'>
-      <i class='fab fa-linkedin-in'></i>
-      <i class='fab fa-github'></i>
-      <i class='fab fa-twitter'></i>
-      {/* <p className='footer-text'>footer component.</p> */}
+      <div className='footer-icons'>
+        <i class='fab fa-linkedin-in'></i>
+        <i class='fab fa-github'></i>
+        <i class='fab fa-twitter'></i>
+      </div>
     </div>
   );
 }
