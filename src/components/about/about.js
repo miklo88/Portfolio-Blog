ca56dfb4f7609f7cb8f4e9caa8c796718c56342c
Carl Redding <credding07@gmail.com>
2020-05-04 00:26:42 -0500

    npm was sucking so i had to uninstall and reinstall it. seems like a monthly thing... besides that was at figma and drew up an awesome landing page again lol. filling out skeleton of about me page first. making element level semantic, filling up the classnames and going to touch with a little bit of styling.

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,23 +1,57 @@
 import React from "react";
-import "./About.scss";
+// import "./About.scss";
 
 const About = () => {
   return (
     <div className='about-container'>
       {/* page title */}
-      <h1>Hi!, I'm Carl!</h1>
-      {/* about me blurb */}
-      <p className='about-text'>something about me.</p>
-      {/* resume download link */}
-      <a href='resume/CARL-REDDING-RESUME.pdf' download='CarlsResume.pdf'>
-        PDF Resume
-      </a>
-      <a href='resume/CARL-REDDING-RESUME.docx' download='CarlsResume.docx'>
-        Word Doc Resume
-      </a>
-
-      {/* what should i put on this page? */}
-      {/* a lil blurb about me, my resume and a link to download my resume. */}
+      <h1 className='About-h1'>About</h1>
+      <div className='title-head'>
+        <p className='who'>who’s this guy?</p>
+        <p className='about-text'> Carl Redding aka carlitos, aka miklo</p>
+        <p className='what'>What do I do?</p>
+        <p className='about-text'>
+          Full Stack Web Developing Student at Lambda why ? im obsessed with
+          stealing moments from people. dancing with color, typography, design,
+          code, CREATING. code has become my platform to dissimenate what goes
+          on in my head to a broader audience than the cat i live with, Diesel.
+        </p>
+        <p className='why'>Why does he do it? </p>
+        <p className='about-text'>
+          I’m obsessed with stealing moments from people. Dancing with color,
+          typography, design, photography, code. CREATING. Coding has become a
+          way for me logically solve problems by simple design and syntax.
+        </p>{" "}
+      </div>
+      <div className='text-body'>
+        <p className='about-text'>
+          loves = [ “salsa dancing”, “team driven debauchery”, wholeness from
+          learning and teaching”, the art of running”, late night problem
+          solving”, the stackoverflow gods who answer questions”, seasonal
+          tasting menus with a heavy pour of wine knowledge”, to visually invoke
+          your attention”, chicago summers” ]
+        </p>
+        <p className='about-text'>
+          hates = [ “open mouth chewers”, “meanies” ]
+        </p>
+      </div>
+      <div className='resume-download'>
+        {/* resume download link */}
+        <a
+          className='resume-link'
+          href='resume/CARL-REDDING-RESUME.pdf'
+          download='CarlsResume.pdf'
+        >
+          PDF Resume
+        </a>
+        <a
+          className='resume-link'
+          href='resume/CARL-REDDING-RESUME.docx'
+          download='CarlsResume.docx'
+        >
+          Word Doc Resume
+        </a>
+      </div>
     </div>
   );
 };
