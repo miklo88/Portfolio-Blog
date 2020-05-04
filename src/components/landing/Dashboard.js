57b8d0bf18dd0210c981d6065459690dc4b5b90a
Carl Redding <credding07@gmail.com>
2020-05-04 01:17:49 -0500

    changed up the landing page. still work to be done but looks great. have to play with the color overlay. and alignment. to grid or not to grid?

--- src/components/landing/Dashboard.js
+++ src/components/landing/Dashboard.js
@@ -8,13 +8,12 @@ const Dashboard = () => {
   return (
     <div className='dashboard-container'>
       <div className='landing'>
-        <img className='dashboard-img-landing' src={landing} alt='dash-img' />
-        <p className='dash-p-landing'>
-          Web Developing,<br></br>
-          Visual Design,<br></br>
-          Chicago, IL
-        </p>
+        <div className='color-overlay'>
+          <img className='dashboard-img-landing' src={landing} alt='dash-img' />
+        </div>
+        <h1 className='dash-p-landing'>hi</h1>
       </div>
+      <p className='welcome'>welcome to my portfolio page</p>
     </div>
   );
 };
