0536cc7860d69b224fc9fb8ef32551c5c0c3d82b
Carl Redding <credding07@gmail.com>
2020-05-04 13:41:07 -0500

    links to each icon work just fine and layed out decent for now.

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -5,9 +5,15 @@ export default function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-icons'>
-        <i class='fab fa-linkedin-in'></i>
-        <i class='fab fa-github'></i>
-        <i class='fab fa-twitter'></i>
+        <a href='https://www.linkedin.com/in/carl-redding-000a4281/'>
+          <i class='fab fa-linkedin-in'></i>
+        </a>
+        <a href='https://github.com/miklo88'>
+          <i class='fab fa-github'></i>
+        </a>
+        <a href='https://twitter.com/Miklo13F'>
+          <i class='fab fa-twitter'></i>
+        </a>
       </div>
     </div>
   );
