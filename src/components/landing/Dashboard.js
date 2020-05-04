bc5f488437ea67fce72db12155aa69f77206bbd1
Carl Redding <credding07@gmail.com>
2020-05-04 11:39:37 -0500

    updating and cleaning mixins still. cleaning house on code DRY 2020. adjusting font on landing page

--- src/components/landing/Dashboard.js
+++ src/components/landing/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import landing from "../../stockPhotos/damePink.png";
+import landing from "../../stockPhotos/dameYellow.png";
 
 import "./Dashboard.scss";
 
@@ -8,9 +8,8 @@ const Dashboard = () => {
   return (
     <div className='dashboard-container'>
       <div className='landing'>
-        <div className='color-overlay'>
-          <img className='dashboard-img-landing' src={landing} alt='dash-img' />
-        </div>
+        <img className='dashboard-img-landing' src={landing} alt='dash-img' />
+
         <h1 className='dash-p-landing'>hi</h1>
       </div>
       <p className='welcome'>welcome to my portfolio page</p>
