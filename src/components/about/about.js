ecd9dfc76dccd26f5aab6249dbeb069b4eeddf27
Carl Redding <credding07@gmail.com>
2020-05-06 15:53:34 -0500

    make js file more semantic. tweaking the about me page more. layout is coming together

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,36 +8,31 @@ const About = () => {
       <div className='about-body'>
         <h1 className='about-h1'>About</h1>
         <div className='title-head'>
-          <p className='who'>
-            Who is this guy?
-            <p className='who-text'> Carl Redding aka carlitos, aka miklo</p>
+          <em className='who'>Who is this guy?</em>
+          <p className='who-text'> Carl Redding aka carlitos, aka miklo</p>
+
+          <em className='what'>What do I do? </em>
+          <p className='what-text'>
+            I’m Full Stack Web Developing Student at Lambda.
           </p>
-          <p className='what'>
-            What do I do?
-            <p className='what-text'>
-              I’m Full Stack Web Developing Student at Lambda.
-            </p>
-          </p>
-          <p className='why'>
-            Why do I do it?
-            <p className='why-text'>
-              I’m obsessed with stealing moments from people. Dancing with
-              color, typography, design, photography, code. CREATING. Coding has
-              become a way for me logically solve problems by simple design and
-              syntax.
-            </p>
+
+          <em className='why'>Why do I do it?</em>
+          <p className='why-text'>
+            I’m obsessed with stealing moments from people. Dancing with color,
+            typography, design, photography, code. CREATING. Coding has become a
+            way for me logically solve problems by simple design and syntax.
           </p>
         </div>
       </div>
       <div className='loves-links'>
         <p className='about-text'>
-          loves = [ “salsa dancing”, “team driven debauchery”, wholeness from
-          learning and teaching”, the art of running”, late night problem
-          solving”, the stackoverflow gods who answer questions”, seasonal
-          tasting menus with a heavy pour of wine knowledge”, to visually invoke
-          your attention”, chicago summers” ]
+          <em>loves</em> = [ “salsa dancing”, “team driven debauchery”,
+          wholeness from learning and teaching”, the art of running”, late night
+          problem solving”, the stackoverflow gods who answer questions”,
+          seasonal tasting menus with a heavy pour of wine knowledge”, to
+          visually invoke your attention”, chicago summers” ]
           <br />
-          hates = [ “open mouth chewers”, “meanies” ]
+          <strong>hates</strong> = [ “open mouth chewers”, “meanies” ]
         </p>
 
         <div className='resume-download'>
