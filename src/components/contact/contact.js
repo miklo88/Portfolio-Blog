0c2ac48a0166381c756efe55c291fdd67f4a3228
Carl Redding <credding07@gmail.com>
2020-05-07 13:21:46 -0500

    basic form for contacting me has been built out.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,6 +2,26 @@ import React from "react";
 // import "contact.scss";
 
 const Contact = () => {
-  return <div>Contact meeee </div>;
+  return (
+    <div className='contact-container'>
+      <form className='contact-form'>
+        <label className='name'>
+          Name:
+          <input className='contact-input' type='text' placeholder='Name' />
+        </label>
+        <label className='email'>
+          Email:
+          <input className='contact-input' type='email' placeholder='Email' />
+        </label>
+        <label className='email'>
+          Message:
+          <input className='contact-input' type='text' placeholder='Message' />
+        </label>
+        <button className='submit-contact' type='submit'>
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 export default Contact;
