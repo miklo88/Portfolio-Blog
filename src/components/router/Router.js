d43fae2824c8d70952984fa415577ddcdfe44f7e
Carl Redding <credding07@gmail.com>
2020-05-07 16:35:43 -0500

    this is too much fun, using react hooks and rendering my contact me component into a pop up module instead of another boring static component

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -6,8 +6,11 @@ import Blog from "../blog/blog";
 // import Signup from "../register/Signup";
 // import Login from "../register/Login";
 import Contact from "../contact/contact";
+import usePopup from "../../utils/custom-hooks/usePopup";
 
 const Router = (props) => {
+  const { isShowing, toggle } = usePopup();
+
   return (
     <>
       <Switch>
@@ -16,7 +19,11 @@ const Router = (props) => {
         <Route exact path='/blog' component={Blog} />
         {/* <Route exact path='/signup' component={Signup} />
         <Route exact path='/login' component={Login} /> */}
-        <Route exact path='/contact' component={Contact} />
+        {/* <Route exact path='/contact' component={Contact} /> */}
+        <button className='contact-btn' onClick={toggle}>
+          Contact
+        </button>
+        <Contact isShowing={isShowing} hide={toggle} />
       </Switch>
     </>
   );
