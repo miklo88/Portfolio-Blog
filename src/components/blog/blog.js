6d1ec2ccc69bf7a50d83055b4eddfcda5d5bc496
Carl Redding <credding07@gmail.com>
2020-05-07 12:38:12 -0500

    added placeholder image and basic styilng out for blog component with text and title completed.

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,14 +1,18 @@
 import React from "react";
+import wip from "../../stockPhotos/doodle1.JPG";
 import "./blog.scss";
 
 const Blog = () => {
   return (
-    <div className='photos-container'>
-      <div className='another-container'>
-        <p>
-          Photos component. This will be styled with scss and a grid layout.
+    <div className='blog-container'>
+      <h1 className='blog-title'>Blog</h1>
+      <div className='message-container'>
+        <p className='wip'>
+          This blog is still a work in progess. To see updates on how I create
+          it follow me on Linkedin and twitter.
         </p>
       </div>
+      <img className='wip-img' src={wip} alt='placement-holder' />
     </div>
   );
 };
