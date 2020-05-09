63094b9bef0372cf71eb28a966925dfacfd6f51d
Carl Redding <credding07@gmail.com>
2020-05-09 16:59:49 -0500

    contact form cleaned out.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,31 +9,6 @@ const Contact = ({ isShowing, hide }) =>
     ? ReactDOM.createPortal(
         <React.Fragment>
           <ContactForm />
-          {/* <div className='contact-container'>
-            <form className='contact-form'>
-              <h1>Can't wait to hear from you!</h1>
-              <label className='name'></label>
-              Name:
-              <input className='contact-input' type='text' placeholder='Name' />
-              <label className='email'></label>
-              Email:
-              <input
-                className='contact-input'
-                type='email'
-                placeholder='Email'
-              />
-              <label className='message'></label>
-              Message:
-              <textarea className='contact-input' placeholder='Message' />
-              <button
-                type='button'
-                className='submit-contact'
-                onClick="alert('message sent')"
-              >
-                Send
-              </button>
-            </form>
-          </div> */}
         </React.Fragment>,
         document.body
       )
