2ec56f1a066c94023dc992a7893cc18a1216adc5
Carl Redding <credding07@gmail.com>
2020-05-10 20:30:23 -0500

    css grid styling of contact form is pretty much scaffolded out. site is pretty much set. moving onto projects tab.

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -4,27 +4,28 @@ import "../contact/contact.scss";
 const ContactForm = () => {
   return (
     <div id='contact-container' className='contact-container'>
+      <h1 className='contact-title'>Can't wait to hear from you!</h1>
       <form
         className='contact-form'
         action='mailto:miklo.concepcion@gmail.com'
         method='post'
         encType='text/palin'
       >
-        <h1 className='contact-title'>Can't wait to hear from you!</h1>
-        <label className='contact-label'>
-          Name:
-          <input className='contact-input' type='text' placeholder='Name' />
-        </label>
-        <label className='contact-label'>
-          Email:
-          <input className='contact-input' type='email' placeholder='Email' />
-        </label>
-        <label className='contact-label'>
-          Message:
-          <input className='contact-input' placeholder='Message' />
-        </label>
+        {/* label*/}
+        <label className='name-label'>Name:</label>
+        {/* input */}
+        <input className='name-input' type='text' placeholder='Name' />
+        {/* label*/}
+        <label className='email-label'>Email:</label>
+        {/* input */}
+        <input className='email-input' type='email' placeholder='Email' />
+        {/* label*/}
+        <label className='message-label'>Message:</label>
+        {/* input */}
+        <input className='message-input' placeholder='Message' />
+        {/* input btns */}
         <input type='submit' className='submit-contact' value='Send' />
-        <input type='reset' className='submit-contact' value='Reset' />
+        <input type='reset' className='reset-contact' value='Reset' />
       </form>
     </div>
   );
