11c4a29259e4e28b7f0779161f1979fca20285c3
Carl Redding <credding07@gmail.com>
2020-05-10 21:37:06 -0500

    created projects component and going to set about my logic of how i'm going to pull githubs api and load three projects.

---
+++ src/components/projects/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className='projects-container'>
+      <h1>I'm a projects component</h1>
+    </div>
+  );
+};
+
+export default Projects;
