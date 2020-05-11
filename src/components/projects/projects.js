faa4fa2c8ea465ae14e69211e10ed7674d068440
Carl Redding <credding07@gmail.com>
2020-05-10 21:39:40 -0500

    scaffolding layout of component.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,6 +4,15 @@ const Projects = () => {
   return (
     <div className='projects-container'>
       <h1>I'm a projects component</h1>
+      <section className='project-one-container'>
+        <p>one</p>
+      </section>
+      <section className='project-two-container'>
+        <p>two</p>
+      </section>
+      <section className='project-three-container'>
+        <p>three</p>
+      </section>
     </div>
   );
 };
