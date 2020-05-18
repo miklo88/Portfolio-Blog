daff33c0a09f04c548c3ec62b662cc697ffb4160
Carl Redding <credding07@gmail.com>
2020-05-18 12:24:17 -0500

    back to getting a body object that is an array. now to destructure it.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-const Projects = () => {
+function Projects() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  // const [repos, setRepos] = useState("");
 
   //componentDidMount // componentDidUpdate
   useEffect(() => {
@@ -11,21 +10,22 @@ const Projects = () => {
       const url = "https://api.github.com/users/miklo88/repos";
       const response = await fetch(url);
       const data = await response.json();
-      const item = data.response;
+      const item = data.results;
       setData(item);
       setLoading(false);
       console.log(data);
     }
     fetchMyAPI();
   }, []);
+
   return (
     <div className='project-container'>
       <section className='project-one-container'>
-        {/* <ul>{loading ? <div>...loading up</div> : <li>{data}</li>}</ul> */}
+        <ul>{loading ? <div>...loading up</div> : <li>{data}</li>}</ul>
         <p>one</p>
       </section>
       <section className='project-two-container'>
-        <ul>{loading ? <div>...loading up</div> : <li>{data}</li>}</ul>
+        {/* <ul>{loading ? <div>...loading up</div> : <li>{data}</li>}</ul> */}
         <p>two</p>
       </section>
       <section className='project-three-container'>
@@ -33,6 +33,22 @@ const Projects = () => {
       </section>
     </div>
   );
-};
+}
 
 export default Projects;
+
+// const [repos, setRepos] = useState("");
+
+//componentDidMount // componentDidUpdate
+// useEffect(() => {
+//   async function fetchMyAPI() {
+//     const url = "https://api.github.com/users/miklo88/repos";
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     const item = data.response;
+//     setData(item);
+//     setLoading(false);
+//     console.log(data);
+//   }
+//   fetchMyAPI();
+// }, []);
