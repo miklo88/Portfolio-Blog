f32452364ead9f267e8a3ad8b883ef929ffda7ae
Carl Redding <credding07@gmail.com>
2020-05-22 13:50:22 -0500

    cleaning up contact me form. Mobile style is set, super simple. tablet and greater is being set up now.

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "../contact/contact.scss";
+// import left from "../../stockPhotos/PIv2.1.jpg";
+// import right from "../../stockPhotos/WEBSITE.png";
 
 const ContactForm = () => {
   return (
     <div id='contact-container' className='contact-container'>
-      <h1 className='contact-title'>Can't wait to hear from you!</h1>
+      <h1 className='contact-title'>Let's create something together.</h1>
       <form
         className='contact-form'
         action='mailto:credding07@gmail.com'
         method='post'
         encType='text/palin'
       >
-        <label className='name-label'>Name:</label>
+        <div className='left-img'>img-mock</div>
+        {/* <img className='left-img' src={left} alt='placement-holder' /> */}
         <input className='name-input' type='text' placeholder='Name' />
-        <label className='email-label'>Email:</label>
         <input className='email-input' type='email' placeholder='Email' />
-        <label className='message-label'>Message:</label>
         <input className='message-input' placeholder='Message' />
 
         <input type='submit' className='submit-contact' value='Send' />
+        {/* <img className='right-img' src={right} alt='placement-holder' /> */}
+        <div className='right-img'>img-mock</div>
       </form>
     </div>
   );
