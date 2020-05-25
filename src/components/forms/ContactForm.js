e767a6f8a0068eecddd750abd56ce28199943f83
Carl Redding <credding07@gmail.com>
2020-05-24 23:55:32 -0500

    selected all of the styles and restyling the page. so far so good. the js file was changed  up a little bit as well. This feels a lot better.

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -6,16 +6,16 @@ const ContactForm = () => {
   return (
     <div id='contact-container' className='contact-container'>
       <h1 className='contact-title'>Let's create something together.</h1>
+      <img className='left-img' src={left} alt='placement-holder' />
       <form
         className='contact-form'
         action='mailto:credding07@gmail.com'
         method='post'
         encType='text/palin'
       >
-        <img className='left-img' src={left} alt='placement-holder' />
-        <input className='name-input' type='text' placeholder='Name' />
-        <input className='email-input' type='email' placeholder='Email' />
-        <input className='message-input' placeholder='Message' />
+        <input className='input text' type='text' placeholder='Name' />
+        <input className='input email' type='email' placeholder='Email' />
+        <input className='input message' placeholder='Message' />
         {/* input button */}
         <input type='submit' className='submit-contact' value='Send' />
       </form>
