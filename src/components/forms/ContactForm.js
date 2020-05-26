7de2d08b1486c0bf28eb54387d06354963d62808
Carl Redding <credding07@gmail.com>
2020-05-26 13:04:46 -0500

    re-establishing flexbox into the grid styling and having success. just centering the content is the issue.

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -6,21 +6,18 @@ const ContactForm = () => {
   return (
     <div id='contact-container' className='contact-container'>
       <img className='left-img' src={left} alt='placement-holder' />
+      <h1 className='contact-title'>Let's create something together.</h1>
       <form
         className='contact-form'
         action='mailto:credding07@gmail.com'
         method='post'
         encType='text/palin'
       >
-        <h1 className='contact-title'>Let's create something together.</h1>
-
         <input className='input text' type='text' placeholder='Name' />
         <input className='input email' type='email' placeholder='Email' />
         <input className='input message' placeholder='Message' />
         {/* input button */}
-        <button className='submit-contact' type='submit' value='Send'>
-          Send
-        </button>
+        <input className='submit-contact' type='submit' value='Send' />
       </form>
     </div>
   );
