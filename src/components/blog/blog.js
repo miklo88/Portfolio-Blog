ee2fdd70e0dfc4af34fffef3a48c11f9e25746ff
Carl Redding <credding07@gmail.com>
2020-05-27 13:27:05 -0500

    knocked out the links for the blog component.

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -6,17 +6,17 @@ const Blog = () => {
   return (
     <div className='blog-container'>
       <h1 className='blog-title'>Blog</h1>
-      <div className='message-container'>
-        <p className='wip'>
-          This blog is still a work in progess. To see how i'm building it
-          follow me!
-          <a href='https://www.linkedin.com/in/carl-redding' target='blank'>
-            Linkedin
-          </a>
-          and
-          <a href='https://twitter.com/Miklo13F'>Twitter</a>
-        </p>
-      </div>
+      <div className='message-container'></div>
+      <p className='wip'>
+        This blog is still a work in progess. To see how i'm building it follow
+        me!
+        <a href='https://www.linkedin.com/in/carl-redding' target='blank'>
+          Linkedin
+        </a>
+        or
+        <a href='https://twitter.com/Miklo13F'>Twitter</a>
+      </p>
+
       <img className='wip-img' src={wip} alt='placement-holder' />
     </div>
   );
