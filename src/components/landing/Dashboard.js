1d1eabe774cb035a6375b324ed6be8785e909996
Carl Redding <credding07@gmail.com>
2020-05-27 17:00:16 -0500

    playing around with the landing page.

--- src/components/landing/Dashboard.js
+++ src/components/landing/Dashboard.js
@@ -1,16 +1,14 @@
 import React from "react";
-
-import landing from "../../stockPhotos/dameYellow.png";
-
+import landing from "../../stockPhotos/hiLogo.png";
 import "./Dashboard.scss";
 
 const Dashboard = () => {
   return (
     <div className='dashboard-container'>
       <div className='landing'>
-        <img className='dashboard-img-landing' src={landing} alt='dash-img' />
-
-        <h1 className='dash-p-landing'>hi</h1>
+        {/* <img className='dashboard-img-landing' src={landing} alt='dash-img' /> */}
+        <div className='dashboard-img-landing'></div>
+        <img className='dash-p-landing' src={landing} alt='dash-img' />
         <p className='welcome'>welcome to my portfolio page</p>
       </div>
     </div>
