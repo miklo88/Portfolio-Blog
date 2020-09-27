3bf88448be98748a1fb054aebae5d2fa69348f36
Carl Redding <credding07@gmail.com>
2020-09-27 17:15:36 -0500

    event handler enters the navigation compnoent. Just toggling is working on that part still need to style and add content for when clicked. cleaning updating code from whiteboard files to main files now. this is fun. its like in iterations. styling being completely re-implemented aqui. the drop down styling still needs to be completed.

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,46 +4,38 @@ import { Link } from "react-router-dom";
 import "./Nav.scss";
 
 function Nav() {
-  //dropdown menu
-  // const hideMenu = (event) => {
-  //   event.preventDefault();
-  //   //if screen is larger than 1000px dont execute function.
-  //   if (window.innerWidth < 1000) {
-  //     let dropmenu = document.getElementById("menu");
-  //     let navcontainer = document.getElementById("nav-menu");
-  //     dropmenu.classList.toggle("change");
-  //     navcontainer.classList.toggle("show");
-  //     return false;
-  //   }
-  // };
+  // menu toggle
+  function toggleMenu(e) {
+    e.preventDefault();
+    console.log("you clicked menu");
+  }
 
   return (
     <div className='nav-container'>
+      {/* home link */}
       <Link className='home-link' to='/'>
         MIKLO
       </Link>
 
-      {/* dropdown menu */}
-      {/* <nav onClick={hideMenu} className='navigation' id='nav-menu'> */}
-      <nav className='navigation' id='nav-menu'>
+      {/* dropdown menu mobile menu */}
+      <button onClick={toggleMenu} className='mobile-nav' id='mobile-menu'>
+        menu
+      </button>
+      {/* navigation links */}
+      <nav className='nav-tabs' id='nav-tabs'>
         {/* About page */}
         <Link className='links basic-button' to='/about'>
           about
         </Link>
-        {/* link to design pg  */}
+        {/* link to design page  */}
         <Link className='links basic-button' to='/design'>
           design
         </Link>
-        {/* contact pg */}
+        {/* contact page */}
         <Link className='links basic-button' to='/contactForm'>
           contact
         </Link>
       </nav>
-
-      {/* toggle dropdown menu for tablet and mobile */}
-      {/* <button onClick={hideMenu} className='menu' id='menu'>
-        menu
-      </button> */}
     </div>
   );
 }
