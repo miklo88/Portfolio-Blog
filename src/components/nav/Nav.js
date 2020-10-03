b378d36b246743f3d4e5265ad80e3f137d39a0d7
Carl Redding <credding07@gmail.com>
2020-10-03 17:47:53 -0500

    mobile nav working horizontilly quite well but just need to loosen it up going vertically. almost there.

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -8,26 +8,29 @@ function Nav() {
   function toggleMenu(e) {
     e.preventDefault();
     // console.log("you clicked menu");
-    document.getElementById("mobile-menu").classList.toggle("change");
+    document.getElementById("toggle-menu").classList.toggle("change");
     document.getElementById("nav-tabs").classList.toggle("show");
   }
 
+  // here i want to have a un-toggled state of logo and either menu or items if desktop.
+  // on click of menu if menu is tablet or mobile size. s
   return (
     <div className='nav-container'>
+      {/* UN-TOGGLED STATE */}
       {/* home link */}
       <Link className='home-link' to='/'>
         MIKLO
       </Link>
-      {/* dropdown menu mobile menu */}
-      <button onClick={toggleMenu} className='mobile-nav' id='mobile-menu'>
+      <button onClick={toggleMenu} className='toggle-nav' id='toggle-menu'>
         menu
       </button>
-
+      {/* TOGGLED STATE */}
       {/* onclick menu aka dropdown */}
       {/* navigation links */}
       <nav className='nav-tabs' id='nav-tabs'>
-        <button onClick={toggleMenu} className='mobile-nav'>
-          le menu
+        {/* mobile nav menu aka change */}
+        <button onClick={toggleMenu} className='mobile-nav' id='mobile-menu'>
+          menu
         </button>
         {/* About page */}
         <Link className='links basic-button' to='/about'>
