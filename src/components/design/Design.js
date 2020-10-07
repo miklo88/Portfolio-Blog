1c3249159f79f5a59ee06825ce3c5b2614400921
Carl Redding <credding07@gmail.com>
2020-10-06 22:35:40 -0500

    almost have the design homepage looking like the mock image. need to explore expanding re concepting this part of the portfolio.

--- src/components/design/Design.js
+++ src/components/design/Design.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import wip from "../../stockPhotos/doodle1.JPG";
+import wip from "../../stockPhotos/julep.png";
 import "./Design.scss";
 
 function Design() {
   return (
     <div className='Design-container'>
-      <h1 className='Design-title'>Design</h1>
+      <h1 className='Design-title'>DESIGN</h1>
       <span className='design-line'></span>
       <div className='message-container'>
-        {/* <img className='wip-img' src={wip} alt='placement-holder' /> */}
+        <img className='wip-img' src={wip} alt='placement-holder' />
         <p className='design-footnote'>
           Marshalls Landing<br></br>2019|Spring Cocktails<br></br>Mint Julep
         </p>
