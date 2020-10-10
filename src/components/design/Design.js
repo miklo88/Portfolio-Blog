479c4434f20eb0dbd3f2d39d86caca162353f59f
Carl Redding <credding07@gmail.com>
2020-10-09 23:19:18 -0500

    just moved the image to the background of the container. just gotta fiddle the page title and the image text content. i mean place it accordingly.

--- src/components/design/Design.js
+++ src/components/design/Design.js
@@ -1,5 +1,5 @@
 import React from "react";
-import wip from "../../stockPhotos/julep.png";
+// import wip from "../../stockPhotos/julep.png";
 import "./Design.scss";
 
 function Design() {
@@ -8,7 +8,7 @@ function Design() {
       <h1 className='Design-title'>DESIGN</h1>
       <span className='design-line'></span>
       <div className='message-container'>
-        <img className='wip-img' src={wip} alt='placement-holder' />
+        {/* <img className='wip-img' src={wip} alt='placement-holder' /> */}
         <p className='design-footnote'>
           Marshalls Landing<br></br>2019|Spring Cocktails<br></br>Mint Julep
         </p>
