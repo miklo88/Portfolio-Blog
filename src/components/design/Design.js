e7e0a99e285ea155160a93aa196affca0e4239fc
Carl Redding <credding07@gmail.com>
2020-10-10 12:18:40 -0500

    website coming togethergit add . the design page is lined up and looking great. now back to the contact to tweak a couple small things then onto the footer

--- src/components/design/Design.js
+++ src/components/design/Design.js
@@ -1,18 +1,16 @@
 import React from "react";
-// import wip from "../../stockPhotos/julep.png";
 import "./Design.scss";
 
 function Design() {
   return (
     <div className='Design-container'>
-      <h1 className='Design-title'>DESIGN</h1>
-      <span className='design-line'></span>
       <div className='message-container'>
-        {/* <img className='wip-img' src={wip} alt='placement-holder' /> */}
-        <p className='design-footnote'>
-          Marshalls Landing<br></br>2019|Spring Cocktails<br></br>Mint Julep
-        </p>
+        <h1 className='Design-title'>DESIGN</h1>
+        <span className='design-line'></span>
       </div>
+      <p className='design-footnote'>
+        Marshalls Landing<br></br>2019|Spring Cocktails<br></br>Mint Julep
+      </p>
     </div>
   );
 }
