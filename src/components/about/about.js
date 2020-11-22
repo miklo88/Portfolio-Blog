bfc57f0bd0ae486c927110f9a393f4f42ad54da2
Carl Redding <credding07@gmail.com>
2020-11-22 17:20:52 -0600

    resume uploaded as a display element as well. I can import pdfs but then i need more packages to install. this is already an image heavy app so why put the burden on ya know

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,30 +5,20 @@ function About() {
   return (
     <div className='about-container'>
       {/* page title */}
-
       <h1 className='about-title'>ABOUT</h1>
       <span className='about-line'></span>
-      <p className='about-content'>
-        Himenaeos nec venenatis ante arcu cubilia nostra convallis conubia
-        commodo montes. Ad fames bibendum vel dolor molestie in sit, suscipit
-        erat sapien interdum? Natoque per parturient viverra. Orci varius
-        viverra mollis non. Ac mi quam dis aliquet erat curabitur scelerisque
-        imperdiet volutpat sem. Class tellus vitae porttitor non ipsum rhoncus
-        consectetur mauris iaculis augue parturient. Placerat pharetra aliquam
-        ornare mauris ut justo. Vulputate, sagittis mauris nulla ridiculus
-        pellentesque neque quis nibh himenaeos. Cursus. Sociosqu penatibus
-        cubilia orci lacinia rhoncus vel iaculis vivamus odio tristique
-        consectetur metus. Cubilia nisl imperdiet gravida aptent sodales urna.
-        Himenaeos fames dignissim, commodo per imperdiet metus in duis tempus.
-      </p>
-      {/* resumes / need to be updated. */}
+      {/* resume on display */}
+      <div className='about-content'>
+        <img src='resume/Carl-Redding-Resume.jpg' alt='display-window' />
+      </div>
+      {/* resumes / need to be updated. 22-NOV-2020 latest update.*/}
       {/* resume download link */}
       <a
         className='resume-link'
-        href='resume/ScrollResume.pdf'
+        href='resume/Carl-Redding-Resume.pdf'
         download='Carl_Redding_Resume.pdf'
       >
-        PDF Resume
+        Download PDF Resume
       </a>
     </div>
   );
