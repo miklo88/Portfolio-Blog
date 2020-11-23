4bbe35031e9c77f327edf8668812344885b676d1
Carl Redding <credding07@gmail.com>
2020-11-22 22:26:12 -0600

    still trying to appease the netlify gods. at this point I could have built my own endpoint to render this damn thing.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.js";
+import App from "./App.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.scss";
