6f73b1dbd8af5e4a0893640e4c5534b60b3d2856
Carl Redding <credding07@gmail.com>
2020-11-28 17:26:15 -0600

    applying mains changes to master. had to reset master

--- src/components/design/Design.js
+++ src/components/design/Design.js
@@ -1,8 +1,17 @@
 import React from "react";
 //GITHUB REPOS API
-import API from "./Api.js";
+// import API from "./Api.js";
 // <STYLES>
 import "./Design.scss";
+//PHOTOS
+import two from "../../stockPhotos/PENECILLINV1.0.jpg";
+import three from "../../stockPhotos/coffee_L_logo.jpg";
+import four from "../../stockPhotos/Triple_Left.png";
+import five from "../../stockPhotos/Triple_Right.png";
+
+const photos = [two, three, four, five];
+
+console.log(photos);
 
 function Design() {
   return (
@@ -16,8 +25,15 @@ function Design() {
           Marshalls Landing<br></br>2019|Spring Cocktails<br></br>Mint Julep
         </p>
       </div>
+      <h1 className='design-title'>MARSHALLS LANDING</h1>
+      <div className='photos-container'>
+        <img className='two' src={two} alt='two' />
+        <img className='three' src={three} alt='three' />
+        <img className='four' src={four} alt='four' />
+        <img className='five' src={five} alt='five' />
+      </div>
       {/* api component */}
-      <API />
+      {/* <API /> */}
     </>
   );
 }
