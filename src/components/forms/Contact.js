01a5c25899a795ff97f79c0fe9fcfdd8636771a6
Carl Redding <credding07@gmail.com>
2020-11-29 20:07:31 -0600

    contact page updated layout in and working. a lot more simple. forms can be used in other releases for the blog etc. or creating the backend for the blog and photography portfolio extension.

--- src/components/forms/Contact.js
+++ src/components/forms/Contact.js
@@ -1,20 +1,45 @@
 import React from "react";
 import "./Contact.scss";
-import hi from "../../stockPhotos/doodle1.jpeg";
+
 function Contact() {
   return (
-    <div id='contact-container' className='contact-container'>
-      <h1 className='contact-title'>CONTACT</h1>
-      <span className='contact-line'></span>
-      {/* IMAGE AND CONTACT BUTTONS AND SOC MEDIA ICONS */}
-      <div className='contact-body'>
-        <img className='contact-img' src={hi} alt='hi' />
+    <div className='contact-container'>
+      {/* TITLE / SOCIAL MEDIA ICONS / MIKLO INFO */}
+      <div className='contact-body-left'>
+        <h1 className='contact-h1'>
+          Let's work <br />
+          together!
+        </h1>
+        <p className='miklo-info'>Find out who the man behind MIKLO is</p>
+        {/* SOCIAL MEDIA ICONS */}
+        <div className='media-icons'>
+          <a href='https://www.linkedin.com/in/carl-redding-000a4281/'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+          <a href='https://github.com/miklo88'>
+            <i className='fab fa-github-square'></i>
+          </a>
+          <a href='https://instagram.com/miklo34'>
+            <i className='fab fa-instagram-square'></i>
+          </a>
+        </div>
+      </div>
+
+      {/* EMAIL BUTTON */}
+      <div className='contact-body-right'>
+        <p className='miklo-design'>
+          - Photography <br />
+          - Visual Design <br />
+          - Front-end Web Development <br />
+        </p>
+        {/* EMAIL BUTTON */}
         <form
           className='contact-form'
           action='mailto:credding07@gmail.com'
           method='post'
           encType='text/palin'
         >
+          <p className='email-p'>Say hi!</p>
           {/* submit button */}
           <button className='submit-contact' type='submit'>
             <i className='far fa-paper-plane'></i>
