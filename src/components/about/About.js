c5d9ee649a12f6d6386d1ae790634aaa92670fe3
Carl Redding <credding07@gmail.com>
2020-12-01 08:26:37 -0600

    cleaned up typos in about me paragraph

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -12,7 +12,7 @@ function About() {
         A strategic food and wine guru who fell in love with tech while building
         a digital network out of a radio when enlisted in the military. I pursue
         and flourish in high-stress situations. My framework consists of being a
-        highly motivated created problem solver with a decorated background in
+        highly motivated creative problem solver with a decorated background in
         leadership, teamwork and impeccable standards. Enthusiastic about my
         development in Relativity's Fellowship program.
       </p>
@@ -25,6 +25,13 @@ function About() {
       >
         Download PDF Resume
       </a>
+      {/* <p className='miklo-why'>
+        Why MIKLO: <br />
+        The nickname miklo dawned upon my naive ass when I started working at mk
+        the restaurant. Like any place that is successful there is a set
+        checklist of tasks to be done and in what order that they will be
+        accomplished in.
+      </p> */}
     </div>
   );
 }
