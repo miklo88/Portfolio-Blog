bfe428b5c985b04562d2409cbd51164e5fcf43b2
Carl Redding <credding07@gmail.com>
2020-12-09 22:18:27 -0600

    for of loop returning value I want. now for in loop attempt. then onto the github api to test.

--- src/components/about/Whiteboard.js
+++ src/components/about/Whiteboard.js
@@ -2,24 +2,25 @@ import React from "react";
 
 export default function Whiteboard() {
   const hello = "HOLA";
+
   // declaring an object
   const myObj = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10 };
   // breaking it down into an array - getting the keys
   const keys = Object.keys(myObj);
   console.log(keys);
   console.log(keys[2]);
+  console.log(keys.length);
   // looping through keys
   //   comparing array entities
-  //   https://stackoverflow.com/questions/11092280/javascript-compare-items-in-one-array
   for (const key of keys) {
-    if (key === key[6]) {
+    if (key === keys[6]) {
       console.log("found key:", key);
-    } else if (key !== key[6]) {
-      break;
+      // return key;
+    } else if (key !== keys[6]) {
+      continue;
     } else {
-      console.log("try again");
+      console.log("error");
     }
-    // console.log(key);
   }
 
   //   // breaking it down by values - returns an array
