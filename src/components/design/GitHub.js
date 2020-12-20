41f80ec0186c65d68b171b27cf6945b1da8295ec
Carl Redding <credding07@gmail.com>
2020-12-19 21:53:13 -0500

    have to figure out why my error wont correctly display. for the meantime look into state management and your setError()

--- src/components/design/GitHub.js
+++ src/components/design/GitHub.js
@@ -3,9 +3,10 @@ import "./GitHub.scss";
 import WIP from "../../stockPhotos/WIPDesktop.jpg";
 
 export default function API() {
-  const [error, setError] = useState("Error");
+  const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [repos, setRepos] = useState([]);
+
   //functional componentDidMount() the useEffect will only run once
   useEffect(() => {
     // playing with pagination and established how many repos I want to display.
@@ -29,13 +30,13 @@ export default function API() {
   if (error) {
     return (
       <div className='error-message'>
-        <p className='development-p'>
+        <div className='development-p'>
           HTTP 418 <br />
           Oh snap! Looks like something happened behind the scenes. Hang tight
-          while I figure this hot mess out. Thank you for the patience.
-        </p>
+          while my lil crew figures this hot mess out.
+          {error.message}
+        </div>
         <img className='WIP' src={WIP} alt='WIPDesktop' />
-        {error.message}
       </div>
     );
   } else if (!isLoaded) {
@@ -44,9 +45,8 @@ export default function API() {
     return (
       <div className='repo-items'>
         <h1 className='development'>Web Development Projects</h1>
-        {/* UNPACKING THE OWNER URL */}
         {repos.map((repo) => (
-          <p className='repo-item' key={repo.id}>
+          <div className='repo-item' key={repo.id}>
             <p className='item'>{repo.name}</p>
             <img src={repo.blobs_url} alt='blob' />
             <img src={repo.commits_url} alt='blob' />
@@ -54,7 +54,7 @@ export default function API() {
             <p className='item'>{repo.owner.html_url}</p>
             {repo.owner.starred_url}
             {repo.pulls_url}
-          </p>
+          </div>
         ))}
       </div>
     );
