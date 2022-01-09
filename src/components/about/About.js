89660d1e3c35047b5355d94ec9fecfbfeb51047c
Carl Redding <credding07@gmail.com>
2022-01-08 22:24:23 -0600

    still tweaking styles but css grid is feeling good.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,26 +2,18 @@ import React from "react";
 import "./About.scss";
 
 function About() {
+  //create function to change the font sizes or the background color. Color might be cooler.
   return (
-    <>
-      <div className='about-header'>
-        {/* page title */}
-        <h1 className='about-title'>ABOUT</h1>
-        <span className='about-line'></span>
-      </div>
-      <div className='body-container'>
-        <p className='content-title'>MIKLO</p>
-        <div className='about-body'>
-          {/* who you are and what youre doing */}
-          <p className='about-content one'>
-            I'm Carl Redding. I created Miklo so I can have a platform to
-            showcase my passion for design and creating stolen moments. My
-            simplicity in approach is understanding the rules and knowing when
-            to break them.
-          </p>
-        </div>
-      </div>
-    </>
+    <section className='about-container'>
+      {/* page title */}
+      <h1 className='about-title'>About</h1>
+      <p className='content-title'>MIKLO</p>
+      <p className='about-content'>
+        I'm Carl Redding. I created Miklo so I can have a platform to showcase
+        my passion for design and creating stolen moments. My simplicity in
+        approach is understanding the rules and knowing when to break them.
+      </p>
+    </section>
   );
 }
 
